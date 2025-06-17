4163ac3e173794c91878c2e2c42268dde15bec37
John Crumpton <john@johncrumpton.com>
2025-06-17 16:39:41 +0100

    Update date-time.php

--- eventprime/themes/default/event/date-time.php
+++ eventprime/themes/default/event/date-time.php
@@ -11,7 +11,18 @@ if( ! empty( $args->event->em_start_date ) && $ep_functions->ep_show_event_date_
             echo esc_html( $new_start_date );
             ?>
         </span>
-        <?php if( ! empty( $args->event->em_start_time ) && $ep_functions->ep_show_event_date_time( 'em_start_time', $args->event ) ) {?>
+       <?php
+        }
+        if( count( $args->event->child_events ) > 0 || ! empty( $args->event->post_parent ) ) { //if event has recurring events ?>
+            <span class="ep-recurring-modal-icon material-icons-outlined ep-bg-dark ep-text-white ep-rounded-5 ep-p-2 ep-cursor-pointer ep-text-small ep-ml-1 ep-cursor" ep-modal-open="ep-get-other-date" id="ep_event_more_child_dates">event_repeat</span><?php
+        }?>
+    </div>
+
+    <div class="ep-box-col-12 ep-text-muted ep-text-small" id="ep-sl-event-end">
+
+
+
+     <?php if( ! empty( $args->event->em_start_time ) && $ep_functions->ep_show_event_date_time( 'em_start_time', $args->event ) ) {?>
             <span class="ep-text-dark ep-fs-6" id="ep_single_event_start_time"><?php 
                 $event_start_time = $ep_functions->ep_convert_time_with_format( $args->event->em_start_time );
                 if( ! empty( $ep_functions->ep_get_global_settings( 'enable_event_time_to_user_timezone' ) ) ) {
@@ -20,14 +31,9 @@ if( ! empty( $args->event->em_start_date ) && $ep_functions->ep_show_event_date_
                     $event_start_time = $ep_functions->ep_convert_time_with_format( $event_timezone_time );
                 }
                 echo esc_html( $event_start_time );?>
-            </span><?php
-        }
-        if( count( $args->event->child_events ) > 0 || ! empty( $args->event->post_parent ) ) { //if event has recurring events ?>
-            <span class="ep-recurring-modal-icon material-icons-outlined ep-bg-dark ep-text-white ep-rounded-5 ep-p-2 ep-cursor-pointer ep-text-small ep-ml-1 ep-cursor" ep-modal-open="ep-get-other-date" id="ep_event_more_child_dates">event_repeat</span><?php
-        }?>
-    </div>
+      </span>
 
-    <div class="ep-box-col-12 ep-text-muted ep-text-small" id="ep-sl-event-end">
+
         <?php if( $args->event->em_all_day == 1 ) {?>
             <span class="ep-text-small ep-text-uppercase">
                 <?php esc_html_e( 'All Day', 'eventprime-event-calendar-management' );?> 
